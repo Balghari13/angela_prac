0102e856e67c512beb6188fababfbd71cfc73260
Balghari <najmbalghari13@gmail.com>
2023-07-26 12:27:24 +0500

    added app icon

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,11 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.blueGrey,
       body: const Center(
         child: Image(
-          image: NetworkImage('https://plus.unsplash.com/premium_photo-1661763150703-149671deb82c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'),
+          //for image using internet
+          //image: NetworkImage('https://plus.unsplash.com/premium_photo-1661763150703-149671deb82c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'),
+
+       //image form local without internet
+        image: AssetImage('assets/images/diamond.png'),
         ),
       ),
     );
