a9a02cd5ced3c576016495a0d62b227c51fa49a2
Balghari <najmbalghari13@gmail.com>
2023-08-30 10:35:29 +0500

    make basic and static ui for dice app

--- lib/diceApp.dart
+++ lib/diceApp.dart
@@ -6,11 +6,44 @@ class DiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
-        title: Text('DiceApp'),
+        backgroundColor: Colors.red,
+        title: const Text('DiceApp'),
       ),
-      body: Column(
-
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+             Expanded(
+     //by default elevated button have 16 right and left padding.
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+                onPressed: (){},
+                child: const Image(
+                  image: AssetImage('assets/images/dice1.png'),
+                ),
+              ),
+            ),
+            //image assets can be show by both ways.Below is more convenient way
+            Expanded(
+            //  flex: 5,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+    onPressed: (){
+                  print('pressed');
+    },
+                  child: Image.asset('assets/images/dice2.png')),
+            ),
+          ],
+        ),
       ),
     );
   }
