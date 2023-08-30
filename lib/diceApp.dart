9ef98d1de166fdaa15dee7eedaef639b1080e9a7
Balghari <najmbalghari13@gmail.com>
2023-08-30 16:02:19 +0500

    added buttons so dice can change when clicked

--- lib/diceApp.dart
+++ lib/diceApp.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class DiceApp extends StatelessWidget {
-  const DiceApp({super.key});
+class DiceApp extends StatefulWidget {
+   const DiceApp({super.key});
+
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+   var leftDiceNumber = 4;
+   var rightDiceNumber = 2;
+   var leftDiceNumber1 = 1;
+   var rightDiceNumber1 = 3;
+
+   void changeDice1(){
+     setState(() {
+       rightDiceNumber = Random().nextInt(6)+1;
+       leftDiceNumber = Random().nextInt(6)+1;
+     });
+   }
+
+   void changeDice(){
+     setState(() {
+       leftDiceNumber1 = Random().nextInt(6)+1;
+       rightDiceNumber1= Random().nextInt(6)+1;
+     });
+
+   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.red,
       appBar: AppBar(
@@ -12,38 +39,112 @@ class DiceApp extends StatelessWidget {
         title: const Text('DiceApp'),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-             Expanded(
-     //by default elevated button have 16 right and left padding.
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
+            //click on one dice it changes value of one side
+    //         Row(
+    //           //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //           //crossAxisAlignment: CrossAxisAlignment.stretch,
+    //           children: [
+    //              Expanded(
+    //  //by default elevated button have 16 right and left padding.
+    //               child: ElevatedButton(
+    //                 style: ElevatedButton.styleFrom(
+    //                   backgroundColor: Colors.transparent,
+    //                   elevation: 0,
+    //                 ),
+    //                 onPressed: (){
+    //                   setState(() {
+    //                     // +1 because random no generates from 0 to 1 minus from max number.
+    //                     leftDiceNumber = Random().nextInt(6)+1;
+    //                   });
+    //                 },
+    //                 child: Image(
+    //                   image: AssetImage('assets/images/dice$leftDiceNumber.png'),
+    //                 ),
+    //               ),
+    //             ),
+    //             //image assets can be show by both ways.Below is more convenient way
+    //             Expanded(
+    //             //  flex: 5,
+    //               child: ElevatedButton(
+    //                 style: ElevatedButton.styleFrom(
+    //                   backgroundColor: Colors.transparent,
+    //                   elevation: 0,
+    //                 ),
+    // onPressed: (){
+    //                 setState(() {
+    //                   rightDiceNumber = Random().nextInt(6)+1;
+    //                 });
+    // },
+    //                   child: Image.asset('assets/images/dice$rightDiceNumber.png')),
+    //             ),
+    //
+    //           ],
+    //         ),
+    //         SizedBox(height: 10,),
+            //click on any of dice and it will chage value of both dice
+            Row(
+              children: [
+                Expanded(
+                  //by default elevated button have 16 right and left padding.
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                    ),
+                    onPressed: (){
+                      changeDice1();
+                      // setState(() {
+                      //   // +1 because random no generates from 0 to 1 minus from max number.
+                      //   leftDiceNumber = Random().nextInt(6)+1;
+                      //   rightDiceNumber= Random().nextInt(6)+1;
+                      // });
+                    },
+                    child: Image(
+                      image: AssetImage('assets/images/dice$leftDiceNumber.png'),
+                    ),
+                  ),
                 ),
-                onPressed: (){},
-                child: const Image(
-                  image: AssetImage('assets/images/dice1.png'),
+                //image assets can be show by both ways.Below is more convenient way
+                Expanded(
+                  //  flex: 5,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                      ),
+                      onPressed: (){
+                        //remove duplication of code by writing functions.
+                        changeDice1();
+                        // setState(() {
+                        //   rightDiceNumber = Random().nextInt(6)+1;
+                        //   leftDiceNumber = Random().nextInt(6)+1;
+                        // });
+                      },
+                      child: Image.asset('assets/images/dice$rightDiceNumber.png')),
                 ),
-              ),
+
+              ],
             ),
-            //image assets can be show by both ways.Below is more convenient way
-            Expanded(
-            //  flex: 5,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                ),
-    onPressed: (){
-                  print('pressed');
-    },
-                  child: Image.asset('assets/images/dice2.png')),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset('assets/images/dice$leftDiceNumber1.png'),
+                  ),),
+                Expanded(child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset('assets/images/dice$rightDiceNumber1.png'),
+                ))
+              ],
             ),
+            ElevatedButton(onPressed: changeDice, child: Text('Roll'),),
           ],
         ),
+
       ),
     );
   }
