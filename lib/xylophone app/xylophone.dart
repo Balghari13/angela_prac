05be21824dc7a70b02f8f649a4b3317045934fd2
Balghari <najmbalghari13@gmail.com>
2023-09-02 13:50:00 +0500

    refactor ui with custom expanded widget by using function

--- lib/xylophone app/xylophone.dart
+++ lib/xylophone app/xylophone.dart
@@ -9,8 +9,24 @@ class XylophoneApp extends StatelessWidget {
     player.play(AssetSource('note$soundNumber.wav'), );
   }
 
+Expanded customWidget(Color color, int num){
+    return(
+  Expanded(
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+      ),
+      onPressed: (){
+        playSound(num);
+      },
+      child: Text(''),
+    ),
+  ));
+}
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Xylophone App'),
@@ -20,104 +36,112 @@ class XylophoneApp extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.cyan,
-                    ),
-                    onPressed: (){
-                      playSound(1);
-                      // final player = AudioPlayer();
-                      // player.play(AssetSource('note1.wav'));
+                customWidget(Colors.red, 1),
+                customWidget(Colors.green,2),
+                customWidget(Colors.lightBlue,3),
+                customWidget(Colors.orangeAccent,4),
+                customWidget(Colors.lightGreenAccent,5),
+                customWidget(Colors.deepOrange,6),
+                customWidget(Colors.purple,7),
 
-                    },
-                    child: Text('Click Me'),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue
-                    ),
-                    onPressed: (){
-                      playSound(2);
-                      // final player = AudioPlayer();
-                      // player.play(AssetSource('note2.wav'),);
-
-                    },
-                    child: Text('Click Me'),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.purple
-                    ),
-                    onPressed: (){
-                      playSound(3);
-                     // final player = AudioPlayer();
-                     // player.play(AssetSource('note3.wav'));
-                    },
-                    child: Text('Click Me'),
-                  ),
-                ),
-               Expanded(
-                 child: ElevatedButton(
-                   style: ElevatedButton.styleFrom(
-                       backgroundColor: Colors.green
-                   ),
-                    onPressed: (){
-                      playSound(4);
-                      // final player = AudioPlayer();
-                      // player.play(AssetSource('note4.wav'));
-
-                    },
-                    child: Text('Click Me'),
-                  ),
-               ),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.yellow
-                    ),
-                    onPressed: (){
-                      playSound(5);
-                      // final player = AudioPlayer();
-                      // player.play(AssetSource('note5.wav'));
-
-                    },
-                    child: Text('Click Me'),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange
-                    ),
-                    onPressed: (){
-                      playSound(6);
-                      // final player = AudioPlayer();
-                      // player.play(AssetSource('note6.wav'));
-
-                    },
-                    child: Text('Click Me'),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                    ),
-                    onPressed: (){
-                      playSound(7);
-                      // final player = AudioPlayer();
-                      // player.play(AssetSource('note7.wav'));
-
-                    },
-                    child: Text(''),
-
-                  ),
-                ),
+               //  Expanded(
+               //    child: ElevatedButton(
+               //      style: ElevatedButton.styleFrom(
+               //          backgroundColor: Colors.cyan,
+               //      ),
+               //      onPressed: (){
+               //        playSound(1);
+               //        // final player = AudioPlayer();
+               //        // player.play(AssetSource('note1.wav'));
+               //
+               //      },
+               //      child: Text('Click Me'),
+               //    ),
+               //  ),
+               //  Expanded(
+               //    child: ElevatedButton(
+               //      style: ElevatedButton.styleFrom(
+               //          backgroundColor: Colors.blue
+               //      ),
+               //      onPressed: (){
+               //        playSound(2);
+               //        // final player = AudioPlayer();
+               //        // player.play(AssetSource('note2.wav'),);
+               //
+               //      },
+               //      child: Text('Click Me'),
+               //    ),
+               //  ),
+               //  Expanded(
+               //    child: ElevatedButton(
+               //      style: ElevatedButton.styleFrom(
+               //          backgroundColor: Colors.purple
+               //      ),
+               //      onPressed: (){
+               //        playSound(3);
+               //       // final player = AudioPlayer();
+               //       // player.play(AssetSource('note3.wav'));
+               //      },
+               //      child: Text('Click Me'),
+               //    ),
+               //  ),
+               // Expanded(
+               //   child: ElevatedButton(
+               //     style: ElevatedButton.styleFrom(
+               //         backgroundColor: Colors.green
+               //     ),
+               //      onPressed: (){
+               //        playSound(4);
+               //        // final player = AudioPlayer();
+               //        // player.play(AssetSource('note4.wav'));
+               //
+               //      },
+               //      child: Text('Click Me'),
+               //    ),
+               // ),
+               //  Expanded(
+               //    child: ElevatedButton(
+               //      style: ElevatedButton.styleFrom(
+               //          backgroundColor: Colors.yellow
+               //      ),
+               //      onPressed: (){
+               //        playSound(5);
+               //        // final player = AudioPlayer();
+               //        // player.play(AssetSource('note5.wav'));
+               //
+               //      },
+               //      child: Text('Click Me'),
+               //    ),
+               //  ),
+               //  Expanded(
+               //    child: ElevatedButton(
+               //      style: ElevatedButton.styleFrom(
+               //        backgroundColor: Colors.orange
+               //      ),
+               //      onPressed: (){
+               //        playSound(6);
+               //        // final player = AudioPlayer();
+               //        // player.play(AssetSource('note6.wav'));
+               //
+               //      },
+               //      child: Text('Click Me'),
+               //    ),
+               //  ),
+               //  Expanded(
+               //    child: ElevatedButton(
+               //      style: ElevatedButton.styleFrom(
+               //        backgroundColor: Colors.red,
+               //      ),
+               //      onPressed: (){
+               //        playSound(7);
+               //        // final player = AudioPlayer();
+               //        // player.play(AssetSource('note7.wav'));
+               //
+               //      },
+               //      child: Text(''),
+               //
+               //    ),
+               //  ),
               ],
             ),
 
